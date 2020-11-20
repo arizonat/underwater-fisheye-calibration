33b270dac35bd442d56e349b67f250a7cc542f0a
Joy Yu <joysyu@mit.edu>
2020-11-19 20:52:32 -0500

    fisheye calibration

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -11,21 +11,21 @@ MODE = "fisheye" #"fisheye" or "normal"
 
 SQUARE_SIZE = 0.03 #meters
 OUTFILE = "camera_calibrations"
-chessBoardHeight = 5
-chessBoardWidth = 7
+BOARD_HEIGHT = 5
+BOARD_WIDTH = 7
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((chessBoardHeight*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:chessBoardWidth,0:chessBoardHeight].T.reshape(-1,2)*0.03
+objp = np.zeros((1,BOARD_HEIGHT*BOARD_WIDTH,3), np.float32)
+objp[0,:,:2]  = np.mgrid[0:BOARD_WIDTH,0:BOARD_HEIGHT].T.reshape(-1,2)*0.03
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('imgs/*.JPG')
+images = glob.glob('fisheye_imgs/*.png')
 
 for fname in images:
     img = cv2.imread(fname)
@@ -33,7 +33,7 @@ for fname in images:
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (chessBoardWidth,chessBoardHeight),None)
+    ret, corners = cv2.findChessboardCorners(gray, (BOARD_WIDTH,BOARD_HEIGHT),None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -43,8 +43,8 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        # img = cv2.drawChessboardCorners(img, (chessBoardWidth,chessBoardHeight), corners2,ret)
-        # cv2.imshow('img',img)
+        img = cv2.drawChessboardCorners(img, (BOARD_WIDTH,BOARD_HEIGHT), corners2,ret)
+        cv2.imshow('img',img)
         cv2.waitKey(500)
 
 cv2.destroyAllWindows()
@@ -62,13 +62,19 @@ if MODE == "normal":
 
 print("distortion coefficients: ", dist)
 
-print("saving matrices to ", OUTFILE+"_"+MODE+".npz")
-np.savez(OUTFILE, mtx, newcameramtx, dist)
+if MODE == "fisheye":
+    print("saving matrix to ", OUTFILE+"_"+MODE+".npz")
+    np.savez(OUTFILE, mtx, dist)
+else:
+    print("saving matrices to ", OUTFILE+"_"+MODE+".npz")
+    np.savez(OUTFILE, mtx, newcameramtx, dist)
 
 mean_error = 0
 for i in range(len(objpoints)):
     if MODE == "fisheye":
         imgpoints2, _ = cv2.fisheye.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        imgpoints2 = imgpoints2.transpose(1,0,2)
+
     elif MODE == "normal":
         imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
     error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
