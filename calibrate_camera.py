3489a13f13d3ae8040e0383bddc075ed31a61f19
Joy Yu <joysyu@mit.edu>
2020-11-19 19:55:34 -0500

    fix chessboard height and width

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -1,6 +1,9 @@
 # initial camera calibration
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html
 
+chessBoardHeight = 5
+chessBoardWidth = 7
+
 import sys
 import os
 import numpy as np
@@ -11,14 +14,15 @@ import glob
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((5*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)*0.03
+objp = np.zeros((chessBoardHeight*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:chessBoardWidth,0:chessBoardHeight].T.reshape(-1,2)*0.03
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('imgs/*.jpg')
+images = glob.glob('imgs/*.JPG')
+
 
 for fname in images:
     img = cv2.imread(fname)
@@ -26,7 +30,7 @@ for fname in images:
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (chessBoardWidth,chessBoardHeight),None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -36,7 +40,7 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+        img = cv2.drawChessboardCorners(img, (chessBoardWidth,chessBoardHeight), corners2,ret)
         cv2.imshow('img',img)
         cv2.waitKey(500)
 
