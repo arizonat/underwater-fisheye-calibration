4835d237d3311101ae965ae91d5cda0b0b46dac0
Levi Cai <cail@mit.edu>
2020-12-05 17:09:03 -0500

    Implemented version where rays are projected onto the calibration target itself for error calc, and saved first result, seems to be not great

--- uw_calibrate_camera.py
+++ uw_calibrate_camera.py
@@ -20,7 +20,7 @@ calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv2.fisheye.CALIB_CHEC
 
 # Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((1,BOARD_HEIGHT*BOARD_WIDTH,3), np.float32)
-objp[0,:,:2]  = np.mgrid[0:BOARD_WIDTH,0:BOARD_HEIGHT].T.reshape(-1,2)*0.03
+objp[0,:,:2]  = np.mgrid[0:BOARD_WIDTH,0:BOARD_HEIGHT].T.reshape(-1,2)*SQUARE_SIZE
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -86,7 +86,7 @@ def uw_ray_trace_residuals(objPoints, imgPoints, K, D, rvecs, tvecs, params):
     
     # Undistort points
     # TODO: understand these weird dimensions
-    undistortedPoints = cv2.fisheye.undistortPoints(np.expand_dims(imgPoints,axis=0), K, D)
+    undistortedPoints = cv2.fisheye.undistortPoints(np.expand_dims(imgPoints,axis=0), K, D, None, K)
     undistortedPoints = np.squeeze(undistortedPoints)
 
     # Prepare residual output
@@ -144,6 +144,91 @@ def uw_ray_trace_residuals(objPoints, imgPoints, K, D, rvecs, tvecs, params):
     print(np.linalg.norm(res))
     return np.squeeze(res)
 
+# Compute the vector of residuals
+def uw_ray_trace_residuals2(objPoints, imgPoints, K, D, rvecs, tvecs, params):
+    """
+    Function to compute the residuals for img coordinates
+    Projects onto the chessboard itself rather than the outside face of the housing
+    """
+    # Unpack and gather parameters
+    dh, th, nx, ny, nz, ra, rh, rw = params
+
+    n = np.array([nx, ny, nz]).T
+    n = n / np.linalg.norm(n)
+    
+    # Undistort points
+    # TODO: understand these weird dimensions
+    undistortedPoints = cv2.fisheye.undistortPoints(np.expand_dims(imgPoints,axis=0), K, D, None, K)
+    undistortedPoints = np.squeeze(undistortedPoints)
+
+    # Prepare residual output
+    num_points = undistortedPoints.shape[0]
+    res = np.zeros((num_points,1))
+
+    # TODO: vectorize all this, write most things in linear form
+    for i, undistortedPoint in enumerate(undistortedPoints):
+        # Project initial ray
+        fx = K[0,0]
+        fy = K[1,1]
+        f = np.mean((fx,fy)) # this is wrong, but what is correct? m/px
+        #xra = np.append(undistorted,np.ones((num_points,1)) * f,1)
+        xra = np.append(undistortedPoint, f)
+        xra_n = xra/np.linalg.norm(xra)
+        
+        # Compute angles due to Snell's law
+        theta_a = np.arccos(np.dot(n,xra_n))
+        theta_h = np.arcsin((ra/rh) * np.sin(theta_a))
+        theta_w = np.arcsin((rh/rw) * np.sin(theta_h))
+
+        # Up to housing barrier
+        lambda_a = dh/np.cos(theta_a)
+        xri = lambda_a * xra_n
+
+        # Up to water barrier
+        lambda_h = (dh+th-np.dot(n,xri)) / np.cos(theta_h)
+        xrh_axis = np.cross(n,xra_n) / np.sin(theta_a)
+        xrh_axis = xrh_axis / np.linalg.norm(xrh_axis)
+        xrh = rot_axis_angle(xra_n, xrh_axis, theta_h - theta_a)
+        xrh_n = xrh / np.linalg.norm(xrh)
+        xro = xri + lambda_h * xrh_n
+
+        # Up to calibration target plane
+        xrw_axis = np.cross(n,xrh_n) / np.sin(theta_h)
+        xrw_axis = xrw_axis / np.linalg.norm(xrw_axis)
+        xrw = rot_axis_angle(xrh_n, xrw_axis, theta_w - theta_h)
+        xrw_n = xrw / np.linalg.norm(xrw)
+
+        # Transform calibration target to camera frame
+        objPoint = np.insert(objPoints[i],3,1.)
+        rvec = rvecs[i].flatten()
+        M_ex = np.zeros((3,4))
+        R,J = cv2.Rodrigues(rvec)
+        M_ex[:,:3] = R
+        M_ex[:,3] = tvecs[i].flatten()
+        P_c = np.matmul(M_ex, objPoint)
+
+        target_pd_wf = np.array([0.,0.,1.,1.]) # z-direction + homogenous coordinate
+        target_pd_cf = np.matmul(M_ex, target_pd_wf)
+        target_pd_cf = target_pd_cf / np.linalg.norm(target_pd_cf)
+
+        # Compare p_c with p_c' to get error
+        lambda_w, P_c_p = intersect_ray_plane(xro, xrw_n, P_c, target_pd_cf)
+
+        # is this the correct error?
+        res[i] = np.linalg.norm(P_c - P_c_p)
+    print(params)
+    print(np.linalg.norm(res))
+    return np.squeeze(res)
+
+def intersect_ray_plane(r0, rd, p0, pd):
+    """
+    Returns the distance and point of intersection of a ray and a plane
+    r0 - point on ray, rd - (unit) direction of ray, p0 - point on plane, pd - (unit) normal of plane
+    """
+    rd = rd / np.linalg.norm(rd)
+    pd = pd / np.linalg.norm(pd)
+    lambda_r = np.dot((p0 - r0), pd) / np.dot(rd, pd)
+    return (lambda_r, r0 + lambda_r * rd)
         
 def aa_to_quat(axis, theta):
     return np.insert(axis * np.sin(theta/2.), 0, np.cos(theta/2.))
@@ -180,15 +265,18 @@ def rot_axis_angle(vec, axis, theta):
 def uw_calibrate(objPoints, imgPoints, K, D, rvecs, tvecs, flags=0, criteria=None):
 
     #uw_ray_trace_residuals(objPoints, imgPoints, K, D, rvecs, tvecs, params):
-    uw_res = partial(uw_ray_trace_residuals, objPoints, imgPoints, K, D, rvecs, tvecs)
+    uw_res = partial(uw_ray_trace_residuals2, objPoints, imgPoints, K, D, rvecs, tvecs)
 
     params0 = [0.001, 0.00635, 0, 0, 1, 1, 1.33, 1.495]
+
+    lwrbnd = [0., 0., -1., -1., 0., 0., 0., 0.]
+    uprbnd = [10., 10., 1., 1., 1., 38., 38., 38.]
     
-    params = least_squares(uw_res, params0)
+    params = least_squares(uw_res, params0, bounds=(lwrbnd,uprbnd))
     return params
 
 params = uw_calibrate(objPoints, imgPoints, K, D, rvecs, tvecs)
 
 # see what our final errors and parameters look like
 print(params)
-print(np.linalg.norm(uw_ray_trace_residuals(objpoints, imgpoints, K, D, rvecs, tvecs, params)))
+#print(np.linalg.norm(uw_ray_trace_residuals(objpoints, imgpoints, K, D, rvecs, tvecs, params)))
